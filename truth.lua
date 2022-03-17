d359a9f002a434cc9d8c6149408de4dd472c6576
MyFatherLeft <101841753+MyFatherLeft@users.noreply.github.com>
2022-03-17 18:49:50 -0500

    Add files via upload

---
+++ truth.lua
@@ -0,0 +1,33 @@
+local MyFatherLeft = Instance.new("ScreenGui")
+local TextLabel = Instance.new("TextLabel")
+
+MyFatherLeft.Name = "MyFatherLeft"
+MyFatherLeft.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+MyFatherLeft.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+TextLabel.Parent = MyFatherLeft
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.800000012, -5, 1, -30)
+TextLabel.Size = UDim2.new(0.200000003, 0, 0, 25)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Father Less 😭"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+TextLabel.TextXAlignment = Enum.TextXAlignment.Right
+
+local function EYIIQH_fake_script()
+	local script = Instance.new('LocalScript', TextLabel)
+
+	local apple = Color3.fromRGB(255,255,255)
+	while wait() do
+		local Joe = Color3.fromRGB(math.random(0,255),math.random(0,255),math.random(0,255))
+		for i = 0,1,0.03 do wait()
+			apple = apple:lerp(Joe,i)
+			script.Parent.TextColor3 = apple
+		end
+	end
+end
+coroutine.wrap(EYIIQH_fake_script)()
