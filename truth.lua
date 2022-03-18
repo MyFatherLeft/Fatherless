5718cd6a74843b7992e4c449e31b6809667c6cac
MyFatherLeft <101841753+MyFatherLeft@users.noreply.github.com>
2022-03-17 20:44:37 -0500

    Update truth.lua

--- truth.lua
+++ truth.lua
@@ -1,32 +1,61 @@
+if game.CoreGui:FindFirstChild("MyFatherLeft") then return end
+
 local MyFatherLeft = Instance.new("ScreenGui")
 local TextLabel = Instance.new("TextLabel")
+local ImageButton = Instance.new("ImageButton")
+local UICorner = Instance.new("UICorner")
 
 MyFatherLeft.Name = "MyFatherLeft"
 MyFatherLeft.Parent = game.CoreGui
 MyFatherLeft.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 TextLabel.Parent = MyFatherLeft
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.Position = UDim2.new(0, 0, 1, -50)
-TextLabel.Size = UDim2.new(0, 200, 0, 25)
+TextLabel.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+TextLabel.BorderColor3 = Color3.fromRGB(27, 42, 53)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0, 5, 1, -50)
+TextLabel.Size = UDim2.new(0, 175, 0, 25)
 TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "Father Less 😭"
+TextLabel.Text = "Father Less 😭  |"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextScaled = true
 TextLabel.TextWrapped = true
 TextLabel.TextXAlignment = Enum.TextXAlignment.Left
 
-local function EYIIQH_fake_script()
+ImageButton.Parent = TextLabel
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BackgroundTransparency = 1.000
+ImageButton.Position = UDim2.new(1, -25, 0.5, -12)
+ImageButton.Size = UDim2.new(0, 25, 0, 25)
+ImageButton.Image = "rbxassetid://3424796336"
+
+UICorner.Parent = TextLabel
+
+local function PCJOWCQ_fake_script()
+	local script = Instance.new('LocalScript', ImageButton)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		setclipboard('Roblox.GameLauncher.joinGameInstance(292439477,"'..tostring(game.JobId)..'")')
+		game:GetService("StarterGui"):SetCore("SendNotification", {
+			Title = "Game Instance Code Copied";
+			Text = 'Roblox.GameLauncher.joinGameInstance(292439477,"'..tostring(game.JobId)..'")';
+			Duration = 5;
+		})
+	end)
+end
+coroutine.wrap(PCJOWCQ_fake_script)()
+local function AGNMEYQ_fake_script()
 	local script = Instance.new('LocalScript', TextLabel)
 
 	local apple = Color3.fromRGB(255,255,255)
 	while wait() do
-		local Joe = Color3.fromRGB(math.random(0,255),math.random(0,255),math.random(0,255))
-		for i = 0,1,0.03 do wait()
-			apple = apple:lerp(Joe,i)
-			script.Parent.TextColor3 = apple
+		local start = script.Parent.Text
+		local End = Color3.fromRGB(math.random(0,255),math.random(0,255),math.random(0,255))
+		for i = 0,1,0.03 do
+			wait()
+			apple = apple:lerp(End,i)
+			script.Parent.Text = '<b><font color= "#'..apple:ToHex()..'">Father Less 😭  </font></b>|'
 		end
 	end
 end
-coroutine.wrap(EYIIQH_fake_script)()
+coroutine.wrap(AGNMEYQ_fake_script)()
